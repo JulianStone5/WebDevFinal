b549d7865813679bb288661fed8c50da400d36a4
Julian Stone <jstone@olin.edu>
2021-04-27 14:53:50 -0400

    Can send messages to individual clients

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,8 @@ let socket = io();
 
 const startBtn = document.querySelector('.btn-start');
 const resetBtn = document.querySelector('.btn-reset');
+resetBtn.style.display = "none";
+const playerHdr = document.querySelector('.hdr-player');
 
 startBtn.addEventListener('click', () => {
     socket.emit('startGame');
@@ -15,16 +17,18 @@ socket.on('resetGame', () => {
     resetGame();
 });
 
-socket.on('startGame', () => {
-    startGame();
+socket.on('startGame', (data) => {
+    startGame(data);
 });
 
-function startGame() {
+function startGame(data) {
     startBtn.style.display = "none";
     resetBtn.style.display = "block";
+    playerHdr.innerHTML = "Player " + data["player_num"];
 }
 
 function resetGame() {
     startBtn.style.display = "block";
     resetBtn.style.display = "none";
+    playerHdr.innerHTML = "whatup";
 }
--- server/server.js
+++ server/server.js
@@ -15,17 +15,36 @@ server.listen(port, () => {
     console.log(`Server is up on port ${port}.`)
 });
 
+var player1 = 0, player2 = 0;
+
 io.on('connection', (socket) => {
 	console.log('A user just connected.');
-    io.emit('resetGame');
-    socket.on('disconnect', () => {
-        console.log('A user has disconnected.');
-    });
+    if(player1 == 0) {
+        player1 = socket;
+    } else if(player2 == 0) {
+        player2 = socket;
+    } else {
+        console.log("rip");
+    }
 
     socket.on('startGame', () => {
-        io.emit('startGame');
-        socket.on('resetGame', () => {
-            io.emit('resetGame');
-        });
+        if(player1 != 0 && player2 != 0) {
+            io.sockets.to(player1.id).emit('startGame',{player_num : 1});
+            io.sockets.to(player2.id).emit('startGame',{player_num : 2});
+            socket.on('resetGame', () => {
+                io.emit('resetGame');
+            });
+        }
+        
+    });
+
+    socket.on('disconnect', () => {
+        console.log('A user has disconnected.');
+        io.emit('resetGame');
+        if(socket.id == player1.id) {
+            player1 = 0;
+        } else if(socket.id == player2.id) {
+            player2 = 0;
+        }
     });
 });
